45f015c3eaced2ea5c071287292160759ea7745a
Lance Johnson <lance.johnson@betterup.co>
2019-01-11 20:45:06 -0600

    Remove all boilerplate code that fragment template added from Android Studio

--- app/src/main/java/com/ldj/navigationarchitecturecomponent/MainActivity.kt
+++ app/src/main/java/com/ldj/navigationarchitecturecomponent/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.ldj.navigationarchitecturecomponent
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() , OnFragmentInteractionListener{
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
