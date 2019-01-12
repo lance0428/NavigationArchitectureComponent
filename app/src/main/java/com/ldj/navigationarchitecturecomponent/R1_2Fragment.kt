9aed01d40f4db32ebc58dcf531d8a89bf5cc8d06
Lance Johnson <lance.johnson@betterup.co>
2019-01-11 22:18:37 -0600

    Attempt to transition to other root view
    This didn't work very well because of some strange exit animation that is happening during transition.

--- app/src/main/java/com/ldj/navigationarchitecturecomponent/R1_2Fragment.kt
+++ app/src/main/java/com/ldj/navigationarchitecturecomponent/R1_2Fragment.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_r1_2.*
 
 
 class R1_2Fragment : Fragment() {
@@ -14,5 +16,10 @@ class R1_2Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_r1_2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        jumpTo1_2Button.setOnClickListener { v -> v.findNavController().navigate(R.id.action_global_r2_1Fragment) }
     }
 }
