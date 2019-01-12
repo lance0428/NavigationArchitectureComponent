0908dcae269d6e0fc3458db7c76aa46b6ed25b85
Lance Johnson <lance.johnson@betterup.co>
2019-01-11 22:27:49 -0600

    Use a pending intent to launch over to other route
    This works but has the larger Activity transition that isn't so great.

--- app/src/main/java/com/ldj/navigationarchitecturecomponent/R1_2Fragment.kt
+++ app/src/main/java/com/ldj/navigationarchitecturecomponent/R1_2Fragment.kt
@@ -5,7 +5,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.navigation.NavDeepLinkBuilder
 import kotlinx.android.synthetic.main.fragment_r1_2.*
 
 
@@ -20,6 +20,12 @@ class R1_2Fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        jumpTo1_2Button.setOnClickListener { v -> v.findNavController().navigate(R.id.action_global_r2_1Fragment) }
+        jumpTo1_2Button.setOnClickListener { v ->
+            val pendingIntent = NavDeepLinkBuilder(requireContext())
+                .setGraph(R.navigation.nav_graph)
+                .setDestination(R.id.r2_1Fragment)
+                .createPendingIntent()
+            pendingIntent.send()
+        }
     }
 }
